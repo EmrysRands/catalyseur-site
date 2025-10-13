27f9466f25d60ca9509b3874f4700a915cc3f6dd
Haga Tiana RANDRIANARIJAONA <tiana@optimizeinsight.com>
2025-10-13 18:45:33 +0300

    fix: correction Tailwind build (index.css + config v4)

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 import tailwind from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
 
-/** @type {import('postcss-load-config').Config} */
 export default {
   plugins: [tailwind(), autoprefixer()],
 };
